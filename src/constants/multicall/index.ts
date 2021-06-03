ade016b3342dc43fc9a4d7405b42d7a1a968de79
shaown <34128508+camonanesi123@users.noreply.github.com>
2021-06-04 01:05:02 +0800

    'first'

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,12 @@
+import { ChainId } from 'hexiaoswap-sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x6e5E7aa5877b6134C2D3afd514CC4CDDdb3921B9',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
